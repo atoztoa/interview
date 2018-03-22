0f2bb497a2993a36b89b13f5b4818b6f29105033
Abheesh Suresh Babu <abheesh.babu@centurylink.com>
2018-03-22 13:37:18 -0600

    Implemented solution for creating tree and traversing it.

--- Readme.txt
+++ Readme.txt
@@ -12,7 +12,9 @@ your robot is harvesting fruits from a tree.  trunk and branches are denoted by 
 your robot cannot jump, fly or drop.  it can only move alongside the tree up and down the branches.
 your job is to keep track of how much the fruits weigh.
 
-
+Steps:
+1.  Use the data file to create a tree
+2.  Complete parse tree function to determine the total weight.
 
 Input file format: every line represents a single branch
 
@@ -24,39 +26,21 @@ All following positions are children weights, say x total where 0 represents ano
 
 Next x line(s) represents all children of a current line.  Once all children are accounted for we go on to next branch.  Example
 
+Input 
 0, -1, 0, 2
 1, 0, 0, 0
 2, 1, 10
 3, 1, 11
- 
 
-0, -1, 0, 2 – id 0, parent id -1 which is undefined, has two children one branch (0) and one fruit with weight of 2
+Output: 4
 
-1, 0, 0, 0 – id 1, parent 0, two children branches
+Explaining input:
+0, -1, 0, 2 - id 0, parent id -1 which is undefined, has two children one branch (0) and one fruit with weight of 2
 
-2, 1, 10 – id 2, parent 1, has one fruit that weights 10., represents first branch that is attached to branch with id 1
+1, 0, 0, 0 - id 1, parent 0, two children branches
+
+2, 1, 10 - id 2, parent 1, has one fruit that weights 10., represents first branch that is attached to branch with id 1
 
 3, 1, 11 - id 3, parent id 1, has one fruit that weighs 11, represents the second branch that is attached to branch with id 1.
 
 output: your output should be a sum of all fruits.
-
-
-// ------------------- Abheesh Suresh Babu : Comments ------------------ //
-
-The question has ambiguities.
-
-1. Should the robot actually crawl the tree because the output is just the final weight of all fruits?
-2. Can the input have any lines not representing a branch in the tree? Because if it is a tree, then the robot can reach all branches without jumping, flying or dropping.
-3. What is the expected result for the 'input.txt' file? Is it 4? Problem statement is not complete without that.
-
-4. If the output is just the weight of all fruits, then the following code will do.
-
-result = fileText.split('\n').map((item) => (
-  item.split(',').map(x => parseInt(x.trim())).slice(2).reduce((a, b) => a + b, 0)
-)).reduce((a, b) => a + b, 0);
-
-But here the tree is not traversed, just the fruits are 'picked' from the input.
-
-5. Is the code supposed to do a tree traversal, for the problem to be considered solved?
-
-Thanks
--- program.js
+++ program.js
@@ -1,25 +1,173 @@
-let parseTree = function(fileText) {
-	var result = "put your result here";
-	
-	//your code goes here
-	
-	return result;
+/**
+ * Represent a branch in the tree
+ */
+class Branch {
+  constructor({ id, value, parentId }) {
+    this.id = id;
+    this.value = value;
+    this.parentId = parentId;
+    this.children = [];
+  }
+
+  /**
+   * Add a branch to the current branch.
+   */
+  addChild(branch) {
+    this.children.push(branch);
+  }
+
+  /**
+   * Get the value of this branch
+   */
+  getValue() {
+    return this.value;
+  }
 }
 
-let readFile = function (evt) {
-	let file = evt.target.files[0]; 
+/**
+ * Represent a tree
+ */
+class Tree {
+  constructor(root) {
+    this.root = root;
+  }
 
-	if (file) {
-	  let reader = new FileReader();
-	  reader.onload = function(e) { 
-		document.getElementById('result').innerHTML = parseTree(e.target.result);
-	  }
-	  reader.readAsText(file);
-	} else { 
-	  document.getElementById('result').innerHTML = "Failed to load file";
-	}
+  /**
+   * Traverse the tree and call the callback on each branch
+   */
+  traverse(callback) {
+    const branchStack = [this.root];
+    let current = null;
+
+    while (branchStack.length !== 0) {
+      current = branchStack.pop();
+
+      if (callback) {
+        callback(current);
+      }
+
+      current.children.forEach((branch) => {
+        branchStack.push(branch);
+      });
+    }
+  }
 }
 
-window.addEventListener("load", function () {
-	document.getElementById('fileInput').addEventListener('change', readFile, false);
+/**
+ * Create a tree from branch data.
+ */
+const createTree = (data) => {
+  // Map of branches to easily lookup a branch
+  const branches = {};
+
+  // Root branch
+  let root = null;
+
+  data.split('\n').forEach((item) => {
+    let branchData = item.split(',').map(x => parseInt(x.trim(), 10));
+
+    branchData = {
+      id: branchData[0],
+      parentId: branchData[1],
+      value: branchData.slice(2).reduce((a, b) => a + b, 0),
+    };
+
+    // Create branch
+    const branch = new Branch(branchData);
+
+    // Add to map
+    branches[branchData.id] = branch;
+
+    // Add branch as child of parent
+    if (branchData.parentId in branches) {
+      branches[branchData.parentId].addChild(branch);
+    } else if (branchData.parentId === -1) {
+      root = branch;
+    }
+  });
+
+  return new Tree(root);
+};
+
+const parseTree = (fileText) => {
+  if (fileText.match(/[a-z]/i)) {
+    return 'Invalid Input File!';
+  }
+
+  const tree = createTree(fileText);
+
+  let totalWeight = 0;
+
+  // Traverse the tree and get weight of each fruit
+  tree.traverse((branch) => {
+    totalWeight += branch.getValue();
+  });
+
+  return totalWeight;
+};
+
+const readFile = (evt) => {
+  const file = evt.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      document.getElementById('result').innerHTML = parseTree(e.target.result);
+    };
+
+    reader.readAsText(file);
+  } else {
+    document.getElementById('result').innerHTML = 'Failed to load file';
+  }
+};
+
+/**
+ * Unit Testing
+ */
+const tests = [
+  {
+    input: '0, -1, 5',
+    expected: 5,
+  },
+  {
+    input: `0, -1, 0, 5
+1, 0, 0, 5
+2, 1, 0, 5
+3, 2, 0, 5
+4, 3, 5`,
+    expected: 25,
+  },
+  {
+    input: `0, -1, 0, 0, 0
+1, 0, 2
+2, 0, 0, 1
+3, 0
+4, 2, 1`,
+    expected: 4,
+  },
+  {
+    input: `0, -1, 0, 2
+1, 0, 0, 0
+2, 1, 10
+3, 1, 11`,
+    expected: 23,
+  },
+];
+
+let result = null;
+
+// Run each test
+tests.forEach((test) => {
+  result = parseTree(test.input);
+  console.assert(result === test.expected, {
+    msg: 'Test failed!',
+    input: test.input,
+    expected: test.expected,
+    actual: result,
+  });
+});
+
+window.addEventListener('load', () => {
+  document.getElementById('fileInput').addEventListener('change', readFile, false);
 }, false);
